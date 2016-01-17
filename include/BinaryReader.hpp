cadd29902abc7a65002e5119de127bac3858cf0c
Andrew Harrison <gamefreak1974@gmail.com>
2016-01-17 09:05:58 -0800

    deleted useless comments

--- include/BinaryReader.hpp
+++ include/BinaryReader.hpp
@@ -13,38 +13,29 @@ class BinaryReader
 		void ChangeFile(uint8_t* data, const uint_fast64_t size);
 		void Close();
 
-		// 8-bit (1 byte)
 		bool ReadBool();
 
-		// 8-bit (1 byte)
 		int8_t ReadInt8();
 		uint8_t ReadUInt8();
 
-		// 16-bit (2 bytes)
 		int16_t ReadInt16();
 		uint16_t ReadUInt16();
 
-		// 32-bit (4 bytes)
 		int32_t ReadInt32();
 		uint32_t ReadUInt32();
 
-		// 64-bit (8 bytes)
 		int64_t ReadInt64();
 		uint64_t ReadUInt64();
 
 		#if defined(__GNUC__) && !defined(__MINGW32__) // MingW gives an error - does not appear to support __int128
-		// 128-bit (16 bytes)
 		__int128 ReadInt128();
 		unsigned __int128 ReadUInt128();
 		#endif
 
-		// 32-bit (4 bytes)
 		float ReadFloat32();
 
-		// 64-bit (8 bytes)
 		double ReadFloat64();
 
-		// 128-bit (16 bytes)
 		long double ReadFloat128();
 
 		char* ReadChars(uint_fast64_t bytes);
@@ -52,7 +43,6 @@ class BinaryReader
 
 		std::string ReadString(uint_fast64_t length);
 
-		//uint32_t Read7BitEncodedInt(uint8_t b);
 		uint64_t Read7BitEncodedInt();
 		std::string ReadStringMS();
 
--- include/BinaryWriter.hpp
+++ include/BinaryWriter.hpp
@@ -4,48 +4,33 @@
 #include <stdint.h>
 #include "BinaryLibUtil.hpp" // for Util functions and the file functions in stdio.h (included in fstream)
 
-// if you change this, don't forget to recompile the library!
-#define FLOAT16 long double
-//#define FLOAT16 __float128
-
 class BinaryWriter
 {
 	public:
 		BinaryWriter(std::string s, bool bak = true);
 		void Close();
 
-		// 8-bit (1 byte)
 		bool WriteBool(bool b);
 
-		// 8-bit (1 byte)
 		bool WriteInt8(int8_t c);
 		bool WriteUInt8(uint8_t value);
 
-		// 16-bit (2 bytes)
 		bool WriteInt16(int16_t i);
 		bool WriteUInt16(uint16_t i);
 
-		// 32-bit (4 bytes)
 		bool WriteInt32(int32_t i);
 		bool WriteUInt32(uint32_t i);
 
-		// 64-bit (8 bytes)
 		bool WriteInt64(int64_t i);
 		bool WriteUInt64(uint64_t i);
 
 		#if defined(__GNUC__) && !defined(__MINGW32__) // MingW gives an error - does not appear to support __int128
-		// 128-bit (16 bytes)
 		bool WriteInt128(__int128 i);
 		bool WriteUInt128(unsigned __int128 i);
 		#endif
 
-		// 32-bit (4 bytes)
 		bool WriteFloat32(float value);
-
-		// 64-bit (8 bytes)
 		bool WriteFloat64(double value);
-
-		// 128-bit (16 bytes)
 		bool WriteFloat128(FLOAT16 value);
 
 		bool WriteChars(const char* c, uint64_t bufSize, uint64_t len, uint64_t startpos = 0);
