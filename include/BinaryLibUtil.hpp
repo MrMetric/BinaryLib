5f3537dcc15792ebeb20a9e0621bf8fe96b2d1e4
Andrew Harrison <gamefreak1974@gmail.com>
2016-01-17 08:38:21 -0800

    BinaryLibUtil is a bit tidier now

--- include/BinaryLibUtil.hpp
+++ include/BinaryLibUtil.hpp
@@ -1,19 +1,21 @@
-#ifndef UTIL_H
-#define UTIL_H
+#pragma once
 
-#include <string.h> // for strerror and errno
+#include <cstring> // for strerror and errno
 #include <fstream> // for std::ifstream, remove, and rename
 #include <iostream> // for std::cerr
 
 #define BINARYLIB_VERSION 10
+
+#define FLOAT16 long double
+
+// not supported by clang
+//#define FLOAT16 __float128
 
 class BinaryLibUtil
 {
 	public:
 		static bool fileExists(const char *filename);
 		static bool fileDelete(const char *filename);
-		static bool moveFile(std::string src, std::string dst, bool overwrite);
+		static bool moveFile(const std::string& src, const std::string& dst, const bool overwrite);
 		static uint32_t version();
 };
-
-#endif // UTIL_H
--- src/BinaryLibUtil.cpp
+++ src/BinaryLibUtil.cpp
@@ -4,9 +4,7 @@
 bool BinaryLibUtil::fileExists(const char *filename)
 {
 	std::ifstream ifile(filename);
-	bool exists = (ifile != NULL);
-	//cout << filename << " exists: " << (exists?"yes":"no") << endl;
-	return exists;
+	return ifile != nullptr;
 }
 
 bool BinaryLibUtil::fileDelete(const char *filename)
@@ -16,22 +14,23 @@ bool BinaryLibUtil::fileDelete(const char *filename)
 		std::cerr << "Error deleting " << filename << ": " << strerror(errno) << " (" << errno << ")\n";
 		return false;
 	}
-	//cout << "Deleted " << filename << endl;
 	return true;
 }
 
-bool BinaryLibUtil::moveFile(std::string src, std::string dst, bool overwrite)
+bool BinaryLibUtil::moveFile(const std::string& src, const std::string& dst, const bool overwrite)
 {
-	//cout << "Moving " << src << " to " << dst << endl;
-	if(overwrite && fileExists(dst.c_str()) && !fileDelete(dst.c_str()))
+	if(BinaryLibUtil::fileExists(dst.c_str()))
 	{
-		std::cerr << "Error renaming \"" << src << "\" to \"" << dst << "\" (failed to delete existing file)\n";
-		return false;
-	}
-	else if(BinaryLibUtil::fileExists(dst.c_str()))
-	{
-		std::cerr << "Error renaming \"" << src << "\" to \"" << dst << "\" (file already exists)\n";
-		return false;
+		if(overwrite && !fileDelete(dst.c_str()))
+		{
+			std::cerr << "Error renaming \"" << src << "\" to \"" << dst << "\" (failed to delete existing file)\n";
+			return false;
+		}
+		else
+		{
+			std::cerr << "Error renaming \"" << src << "\" to \"" << dst << "\" (file already exists)\n";
+			return false;
+		}
 	}
 	rename(src.c_str(), dst.c_str());
 	return true;
